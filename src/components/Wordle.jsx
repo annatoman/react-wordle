3e624dcad27c7c6624b8899261ddfbb9e40b406d
Leah Perri <perrileah2023@gmail.com>
2023-04-30 16:06:26 -0500

    adds flip and color animation

--- src/components/Wordle.jsx
+++ src/components/Wordle.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import useWordle from "../hooks/useWordle";
 import Grid from "./Grid";
+import Keypad from "./Keypad";
 
 export default function Wordle({ solution }) {
   const { currentGuess, handleKeyup, guesses, isCorrect, turn } = useWordle(solution);
@@ -19,7 +20,8 @@ export default function Wordle({ solution }) {
     <div>
       <div>solution - {solution}</div>
       <div>current guess - {currentGuess}</div>
-      <Grid currentGuess={currentGuess} guesses={guesses} turn={turn}/>
+      <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
+      <Keypad />
     </div>
   );
 }
