a45e299fd166ce7d9c90d9a7c5a77e0dd9d63c09
Anna <annamtoman@gmail.com>
2023-04-30 16:39:36 -0500

    adds keypad component and css styling

--- src/components/Keypad.jsx
+++ src/components/Keypad.jsx
@@ -1,22 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
 
-export default function Keypad() {
-  const [letters, setLetters] = useState(null);
+export default function Keypad( usedKeys ) {
+  const [letters, setLetters] = useState(null)
 
   useEffect(() => {
-    fetch("http://localhost:3001/letters")
-      .then((res) => res.json())
-      .then((json) => {
-        setLetters(json);
-      });
-  }, []);
+    fetch('http://localhost:3001/letters')
+      .then(res => res.json())
+      .then(json => {
+        setLetters(json)
+      })
+  }, [])
 
   return (
     <div className="keypad">
-      {letters &&
-        letters.map((l) => {
-          return <div key={l.key}>{l.key}</div>;
-        })}
+      {letters && letters.map(l => {
+        const color = usedKeys[l.key]
+        return (
+          <div key={l.key} className={color}>{l.key}</div>
+        )
+      })}
     </div>
-  );
+  )
 }
--- src/hooks/useWordle.jsx
+++ src/hooks/useWordle.jsx
@@ -6,6 +6,7 @@ const useWordle = (solution) => {
   const [guesses, setGuesses] = useState([...Array(6)]); // guess is an array
   const [history, setHistory] = useState([]); // guess is a string
   const [isCorrect, setIsCorrect] = useState(false);
+  const [usedKeys, setUsedKeys] = useState({}) // {a: 'green', b: 'yellow', c: 'gray'}
 
   const formatGuess = () => {
     let solutionArray = [...solution];
@@ -48,6 +49,28 @@ const useWordle = (solution) => {
     setTurn((prevTurn) => {
       return prevTurn + 1;
     });
+    setUsedKeys((prevUsedKeys) => {
+      let newKeys = {...prevUsedKeys}
+
+      formattedGuess.forEach((l) => {
+        const currentColor = newKeys[l.key]
+
+        if (l.color === 'green') {
+          newKeys[l.key] = 'green'
+          return
+        }
+        if (l.color === 'yellow' && currentColor !== 'green') {
+          newKeys[l.key] = 'yellow'
+          return
+        }
+        if (l.color === 'gray' && currentColor !== 'green' && currentColor !== 'yellow') {
+          newKeys[l.key] = 'gray'
+          return
+        }
+      })
+
+      return newKeys
+    })
     setCurrentGuess("");
   };
 
@@ -89,7 +112,7 @@ const useWordle = (solution) => {
     }
   };
 
-  return { turn, currentGuess, guesses, isCorrect, handleKeyup };
+  return { turn, currentGuess, guesses, isCorrect, usedKeys, handleKeyup };
 };
 
 export default useWordle;
